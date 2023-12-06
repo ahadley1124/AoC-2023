d45317e227aabc8733ce69d2f721db94ee206fe9
ahadley1124 <admin@austinh.dev>
2023-12-05 23:28:50 -0500

    Init Day One Rust Project
    Signed-off-by: ahadley1124 <admin@austinh.dev>

---
+++ Day-1/target/debug/deps/Day_1-bf4da53c9823116b.d
@@ -0,0 +1,5 @@
+/Users/austin/Documents/GitHub/AoC-2023/Day-1/target/debug/deps/Day_1-bf4da53c9823116b: src/main.rs
+
+/Users/austin/Documents/GitHub/AoC-2023/Day-1/target/debug/deps/Day_1-bf4da53c9823116b.d: src/main.rs
+
+src/main.rs:
