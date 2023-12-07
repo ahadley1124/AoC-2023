cc266a88c51215bbc7e65b23b4efa87d1602d31e
Austin Hadley <admin@austinh.dev>
2023-12-06 20:50:02 -0500

    Fix ASCII parse Error
    Signed-off-by: Austin Hadley <admin@austinh.dev>

--- Day-1/src/main.rs
+++ Day-1/src/main.rs
@@ -2,11 +2,18 @@ use std::fs::File;
 use std::io::Read;
 
 fn read_input_file() -> Vec<String> {
-    let mut file = File::open("puzzleInput.txt").expect("File not found");
-    let mut lines: Vec<String> = Vec::new();
-    let mut content = String::new();
-    file.read_to_string(&mut content).expect("Something went wrong reading the file");
-    lines.push(content.split("\n").collect());
+    let filename = "puzzleInput.txt";
+    let mut file = match File::open(filename) {
+        Ok(file) => file,
+        Err(_) => panic!("no such file"),
+    };
+    let mut file_contents = String::new();
+    file.read_to_string(&mut file_contents)
+        .ok()
+        .expect("failed to read!");
+    let lines: Vec<String> = file_contents.split("\n")
+        .map(|s: &str| s.to_string())
+        .collect();
     lines
 }
 
@@ -45,22 +52,35 @@ fn find_last_number(line: String) -> String {
 }
 
 fn ascii_to_real_number(ascii: u32) -> i32 {
-    let mut real_number = 0;
-    if ascii >= 48 && ascii <= 57 {
-        real_number = ascii as i32;
+    match ascii {
+        48 => 0,
+        49 => 1,
+        50 => 2,
+        51 => 3,
+        52 => 4,
+        53 => 5,
+        54 => 6,
+        55 => 7,
+        56 => 8,
+        57 => 9,
+        _ => panic!("Not a number"),        
     }
-    real_number
 }
 
 fn main() {
+    //let input = parse_input_file(read_input_file());
     let input = read_input_file();
     let mut sum = 0;
     for line in input {
         let mut first_number: String = find_first_number(line.clone());
         let last_number: String = find_last_number(line.clone());
-        first_number.push_str(&last_number);
-        let number: i32 = first_number.parse().unwrap();
-        sum += number;
+        let mut first_number: u32 = first_number.parse().unwrap();
+        let last_number: u32 = last_number.parse().unwrap();
+        let first_digit = ascii_to_real_number(first_number);
+        let last_digit = ascii_to_real_number(last_number);
+        // append the last digit to the first digit
+        let numbers: i32 = format!("{}{}", first_digit, last_digit).parse().unwrap();
+        sum += numbers;
     }
     println!("Sum: {}", sum);
 }
--- Day-1/target/debug/Day-1.d
+++ Day-1/target/debug/Day-1.d
@@ -1,1 +1,1 @@
-/Users/austin/Documents/GitHub/AoC-2023/Day-1/target/debug/Day-1: /Users/austin/Documents/GitHub/AoC-2023/Day-1/src/main.rs
+E:\github\AoC-2023\Day-1\target\debug\Day-1.exe: E:\github\AoC-2023\Day-1\src\main.rs
